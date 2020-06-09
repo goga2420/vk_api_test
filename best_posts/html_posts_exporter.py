ade5b1e2f7e9ed25db67802e97bdc12892a9c348
goga2420 <58983503+goga2420@users.noreply.github.com>
2020-06-09 14:24:26 +0300

    Добавление лайков в html и рефакторинг html_posts_exporter.py

--- best_posts/html_posts_exporter.py
+++ best_posts/html_posts_exporter.py
@@ -1,19 +1,28 @@
+from best_posts.top_posts_selector import TopPostsSelector
+
+
 class HtmlPostsExporter:
     def __init__(self, wall_id, posts):
         self.wall_id = wall_id
         self.posts = posts
 
-
     def export(self):
-        vk_url = 'https://vk.com/wall'
         head = '<!DOCTYPE HTML>\n<meta charset="utf-8"/>\n<html>\n<head><title>Сылки на посты</title><head/>\n<body>\n'
         string_end = '</body>\n</html>'
-
-        all_links = ''
-        for i in range(0, len(self.posts)):
-            href = '\t<a href="' + vk_url + str(self.wall_id) + '_' + str(self.posts[i]['id']) + '">Пост ' + str(i + 1) + '</a><br/>\n'
-            all_links = all_links + href
-
-        html_string = head + all_links + string_end
+        html_string = head + self.all_posts_link_maker(self) + string_end
 
         return html_string
+
+    def all_posts_link_maker(self):
+        all_links = ''
+        for i in range(0, len(self.posts)):
+           all_links = all_links + self.one_post_link_maker(self, self.posts[i])
+
+        return all_links
+
+    def one_post_link_maker(self, post):
+        vk_url = 'https://vk.com/wall'
+        href = '\t<a href="' + vk_url + str(self.wall_id) + '_' + str(post['id']) + '">' + str(
+            TopPostsSelector(0, 0).extract_likes(post)) + '❤</a><br/>\n'
+
+        return href
